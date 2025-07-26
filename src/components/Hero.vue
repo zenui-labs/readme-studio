e8171fa0da533957568ce0cd1114c9be7eed42e5
Asfak Ahmed ( rahi ) <asfakahmed680@gmail.com>
2025-07-26 11:06:23 +0600

    Add Product Hunt badge to Hero component
    Updated the Hero.vue component to include a Product Hunt featured badge next to the 'Create Readme' button. Also adjusted the vertical margin for improved spacing and grouped the call-to-action and badge in a flex container for better layout.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@ import AiIcon from "@/svg-icons/ai-icon.vue";
 </script>
 
 <template>
-  <div class='max-w-4xl mx-auto my-[100px] md:my-0 px-6 md:px-0'>
+  <div class='max-w-4xl mx-auto my-[150px] md:my-0 px-6 md:px-0'>
     <h1 class="text-[3rem] md:text-[4.5rem] dark:text-darkText leading-[60px] md:leading-[90px] font-bold capitalize z-20">
       Your <span class="text-brandColor uppercase">Projects</span>
       Deserve Better <span class="text-brandColor uppercase">docs</span>
@@ -12,12 +12,20 @@ import AiIcon from "@/svg-icons/ai-icon.vue";
       Let AI do the writing. Readme Studio builds meaningful, professional READMEs tailored to your project.
     </p>
 
-    <router-link
-        to="/generate"
-        class="py-2.5 mt-10 cursor-pointer bg-brandColor/80 hover:bg-brandColor transition-all duration-300 px-6 text-white rounded-lg text-[1.1rem] flex items-center gap-3 justify-center w-max mx-auto"
-    >
-      <AiIcon/>
-      Create Readme
-    </router-link>
+    <div class='flex flex-col md:flex-row items-center mt-10 gap-5 w-max mx-auto'>
+      <router-link
+          to="/generate"
+          class="py-2.5 cursor-pointer bg-brandColor/80 hover:bg-brandColor transition-all duration-300 px-6 text-white rounded-lg text-[1.1rem] flex items-center gap-3 justify-center"
+      >
+        <AiIcon/>
+        Create Readme
+      </router-link>
+
+      <a href="https://www.producthunt.com/products/readme-studio?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-readme&#0045;studio"
+         target="_blank"><img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=996454&theme=light&t=1753506088862"
+          alt="Readme&#0032;Studio - Your&#0032;AI&#0032;Readme&#0032;generator | Product Hunt"
+          style="width: 225px; height: 54px;" width="200" height="54"/></a>
+    </div>
   </div>
 </template>
